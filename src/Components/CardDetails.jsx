a9f64f0ae555613d02ffa18a61741dc90979c7c2
Minahil Muhammad Moiz <minahil4864@gmail.com>
2024-04-19 13:03:17 +0500

    Set items on home page and individual item details

--- src/Components/CardDetails.jsx
+++ src/Components/CardDetails.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import { Delete } from "@mui/icons-material";
+import React from "react";
+import { Table } from "react-bootstrap";
 
 const CardDetails = () => {
   return (
-    <div>CardDetails</div>
-  )
-}
+    <>
+      <div className="container mt-2">
+        <h2>Item Details Page</h2>
+        <section className="container mt-3">
+          <div className="itemsdetails">
+            <div className="items_img">
+              <img src="" alt="" />
+            </div>
+            <div className="details">
+              <Table>
+                <tr>
+                  <td>
+                    <p>
+                      <strong>Restaurant:</strong>
+                    </p>
+                    <p>
+                      <strong>price: ₹</strong>
+                    </p>
+                    <p>
+                      <strong>Dish:</strong>
+                    </p>
+                    <p>
+                      <strong>Total: ₹</strong>
+                    </p>
+                  </td>
+                  <td>
+                    <p><strong>Rating:</strong></p>
+                    <p><strong>Order Review:</strong></p>
+                    <p><strong>Remove:</strong><Delete style={{color: 'red'}}/></p>
+                  </td>
+                </tr>
+              </Table>
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+};
 
-export default CardDetails
+export default CardDetails;
--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Cardsdata from "./CardsData";
 
 const Cards = () => {
-  return (
-    <div className='container mt-3 '>
-        <h1>Add To Cart</h1>
-        <div className="row"></div>
-    </div>
-  )
-}
+  const [data, setData] = useState(Cardsdata);
 
-export default Cards
+  return (
+    <div className="container mt-3 ">
+      <h1>Add To Cart</h1>
+      <div className="row d-flex justify-center align-items-center  ">
+        {data.map((card, id) => (
+          <Card
+            style={{ width: "18rem", border: "none" }}
+            className="m-2 mt-4 card_style"
+          >
+            <Card.Img
+              variant="top"
+              src={card.imgdata}
+              style={{ height: "16rem" }}
+            />
+            <Card.Body>
+              <Card.Title>{card.rname}</Card.Title>
+              <Card.Text>price: {card.price} ₹</Card.Text>
+              <div className="button_div ">
+                <Button variant="primary">Add To Cart</Button>
+              </div>
+            </Card.Body>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
